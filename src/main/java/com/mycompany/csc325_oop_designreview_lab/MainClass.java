1176725fa419cf6b809e62cf070e8bd5c01b1caa
Aurpa <Aurpa@PandaLand>
2023-10-13 20:44:03 -0400

    worked on the TO Do

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -11,34 +11,18 @@ package com.mycompany.csc325_oop_designreview_lab;
 public class MainClass {
 
  public static void main(String[] args) {
-		// ToDo 5: Fix the error
+     
+                 Student std1 = new Student("James", (short) 20);
+                 Freshman fresh1 = new Freshman("James", (short) 20, 3.5);
+                 Senior std2 = new Senior("John", (short)30, 90);
 
-		// ToDo 6: Fix the constructor of the Student class
+                  // ToDo 12: Set the GPA of the student using the scanner and user input and then print the output.
+                 std1.setGpa(3.7);
 
-                // Todo 7: Create two classes for Freshman and Senior 
-
-                // ToDo 8: The senior class should have a minimum of 85 credits  
-
-		// ToDo 9: Add a toString method for the Student class
-		// ToDo 10: Add a toString method for the Freshman class
-
-		Student std1= new Student("James", 20);
-                // ToDo 11: Add a toString method for the Senior class
-
-		Freshman std1= new Student("James", 20, 12); // name, age, credits
-
-                Senior std2 = new Student("John", 30, 90);
-
-		// ToDo 12: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-
-		System.out.println(std1);
-
-                System.out.println(std2);
-
-		// ToDo 13: add comments and explain your code
-
-		// ToDo 14: submit using a pull request.
+                 System.out.println(std1);
+                 System.out.println(fresh1);
+                 System.out.println(std2);
+		
 	}
 
 }
